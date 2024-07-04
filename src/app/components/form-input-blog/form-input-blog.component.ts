40528899186031d3025dbc1e96b2a3422a57226b
Ivan <ivanrhaut@gmail.com>
2024-07-04 15:57:20 +0200

    bindeado ngModels con el formulario

--- src/app/components/form-input-blog/form-input-blog.component.ts
+++ src/app/components/form-input-blog/form-input-blog.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Inoticia } from '../../interfaces/inoticia.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-input-blog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-input-blog.component.html',
   styleUrl: './form-input-blog.component.css'
 })
 export class FormInputBlogComponent {
-
+  newNoticia: Inoticia = {title:"",imageurl:"",body:"",date: new Date()};
 }
